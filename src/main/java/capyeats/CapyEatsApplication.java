225864b9b0eb7cb357fa47a759aef3bfdd96bd07
denislav999 <237266975+denislav999@users.noreply.github.com>
2025-11-17 12:19:51 +0200

    first commit

---
+++ src/main/java/capyeats/CapyEatsApplication.java
@@ -0,0 +1,13 @@
+package capyeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapyEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapyEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capyeats/CapyEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capyeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapyEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
