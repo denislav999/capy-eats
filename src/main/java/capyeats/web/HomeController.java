26d1c14d5c4a28dfbb6efc2b25280e87cc9a9143
denislav999 <237266975+denislav999@users.noreply.github.com>
2025-11-18 13:06:37 +0200

    landing page front-end rdy

--- src/main/java/capyeats/web/HomeController.java
+++ src/main/java/capyeats/web/HomeController.java
@@ -10,5 +10,45 @@ public class HomeController {
     public String home() {
         return "index";
     }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @GetMapping("/press")
+    public String press() {
+        return "press";
+    }
 
+    @GetMapping("/careers")
+    public String careers() {
+        return "careers";
+    }
+    @GetMapping("/resources")
+    public String resources(){
+        return "resources";
+    }
+
+    @GetMapping("/partner")
+    public String partner(){
+        return "partner";
+    }
+
+    @GetMapping("/support")
+    public String support(){
+        return "support";
+    }
+    @GetMapping("/privacy")
+    public String privacy(){
+        return "privacy";
+    }
+
+    @GetMapping("/terms")
+    public String terms(){
+        return "terms";
+    }
+
+    @GetMapping("/cookies")
+    public String cookies(){
+        return "cookies";
+    }
 }
